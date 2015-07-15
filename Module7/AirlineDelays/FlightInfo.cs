1f314bf0a94d4190c38c74d9793e3bd973cb3d50
Sasha Goldshtein <goldshtn@gmail.com>
2015-07-15 18:28:22 +0300

    Initial commit of module 7's AirlineDelays solution

---
+++ Module7/AirlineDelays/FlightInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineDelays
+{
+    class FlightInfo
+    {
+        public string Airline { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int DepartureDelay { get; set; }
+        public int ArrivalDelay { get; set; }
+        public bool Cancelled { get; set; }
+        public int Distance { get; set; }
+
+        public FlightInfo(string[] fields)
+        {
+            Airline = fields[0];
+            Origin = fields[1];
+            Destination = fields[2];
+            DepartureDelay = int.Parse(fields[3]);
+            ArrivalDelay = int.Parse(fields[4]);
+            Cancelled = int.Parse(fields[5]) == 1;
+            Distance = int.Parse(fields[6]);
+        }
+
+        public static List<FlightInfo> ReadFlightsFromFile(string fileName)
+        {
+            List<FlightInfo> flights = new List<FlightInfo>();
+            bool first = true;
+            using (var reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (first)
+                    {
+                        first = false;
+                        continue;
+                    }
+
+                    string[] parts = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length != 7)
+                        continue;
+
+                    flights.Add(new FlightInfo(parts));
+                }
+            }
+            return flights;
+        }
+    }
+}
---
+++ Module7/AirlineDelays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineDelays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FlightInfo> flights = FlightInfo.ReadFlightsFromFile(@"C:\Users\Sasha\Downloads\airline-on-time-performance-sep2014-us.csv");
+            var query = (from flight in flights
+                         where flight.Airline == "AA"
+                         group flight.DepartureDelay by flight.Origin into g
+                         select new { Airport = g.Key, DepartureDelay = g.Average() }
+                         ).OrderByDescending(a => a.DepartureDelay).Take(1);
+
+            if (query is IEnumerable)
+            {
+                foreach (var row in (IEnumerable)(object)query)
+                {
+                    Console.WriteLine(row);
+                }
+            }
+            else
+            {
+                Console.WriteLine(query);
+            }
+        }
+    }
+}
