b95a78aa8f34c429587ea288eecfe093400557cc
Sasha Goldshtein <goldshtn@gmail.com>
2015-07-16 17:29:44 +0300

    wrote the logic to transform card image location

--- Module8/Blackjack/Blackjack/MainWindow.xaml.cs
+++ Module8/Blackjack/Blackjack/MainWindow.xaml.cs
@@ -23,6 +23,41 @@ namespace Blackjack
         public MainWindow()
         {
             InitializeComponent();
+
+            string name;
+        }
+
+        private string GetImageFileNameForCard(Card card)
+        {
+            int col = -1;
+            switch (card.Suit)
+            {
+                case "Spades":
+                    col = 0;
+                    break;
+                case "Clubs":
+                    col = 1;
+                    break;
+                case "Hearts":
+                    col = 2;
+                    break;
+                case "Diamonds":
+                    col = 3;
+                    break;
+            }
+
+            int row = -1;
+            
+            if (card.Rank == "Ace")
+            {
+                row = 0;
+            }
+            else
+            {
+                row = 14 - card.GetValue();
+            }
+
+            return "Images/" + (row * 4 + col + 1).ToString() + ".png";
         }
     }
 }
