451b9a1f6ecc39309737c25225d82d8ed8cdfc47
Sasha Goldshtein <goldshtn@gmail.com>
2015-07-16 09:06:48 +0300

    Added practice questions for Module 3

---
+++ Module3/PracticeQuestions/PracticeQuestions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = -43;
+            if (value >= 0)
+            {
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine(-value);
+            }
+
+            for (int i = 1; i <= 100; ++i)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i + " Buzz!");
+                }
+            }
+
+            int power = 2;
+            for (int i = 1; i <= 10; ++i)
+            {
+                Console.WriteLine(power);
+                power = power * 2;
+            }
+
+            int sum = 0;
+            for (int i = 1; i <= 100; ++i)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
