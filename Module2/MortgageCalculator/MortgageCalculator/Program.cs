bab71541b44c7787c2d9418f05a1273ab9f2e367
Sasha Goldshtein <goldshtn@gmail.com>
2015-07-16 17:02:00 +0300

    added forgotten files

---
+++ Module2/MortgageCalculator/MortgageCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the house price: ");
+            string priceInput = Console.ReadLine();
+            int price = int.Parse(priceInput);
+
+            Console.Write("Please enter the mortage duration in years: ");
+            string yearsInput = Console.ReadLine();
+            int years = int.Parse(yearsInput);
+
+            Console.Write("Please enter the yearly interest rate in %: ");
+            string interestPercentInput = Console.ReadLine();
+            int interestPercent = int.Parse(interestPercentInput);
+
+            double interestRate = 1 + (interestPercent / 100.0);
+
+            double total = price * Math.Pow(interestRate, years);
+
+            Console.WriteLine("Your total mortage amount will be: " + (int)total);
+        }
+    }
+}
---
+++ Module7/WordCount/WordCount/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    class Program
+    {
+        static List<string> AllWords(string filename)
+        {
+            List<string> words = new List<string>();
+            foreach (string line in File.ReadAllLines(filename))
+            {
+                foreach (string word in line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    words.Add(word.Trim(',', '.', '-', '!', '?', '\'', '"', ';', '(', ')', '[', ']').ToLower());
+                }
+            }
+            return words;
+        }
+
+        static Dictionary<string, int> WordFrequencies(List<string> words)
+        {
+            Dictionary<string, int> frequencies = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                if (frequencies.ContainsKey(word))
+                    frequencies[word] += 1;
+                else
+                    frequencies[word] = 1;
+            }
+            return frequencies;
+
+            // Here is an alternative implementation, expressed as a LINQ query and the ToDictionary method:
+            //return (from word in words
+            //        group 1 by word into g
+            //        select new { Word = g.Key, Freq = g.Count() }).ToDictionary(a => a.Word, a => a.Freq);
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var wordAndFrequency in
+                (from wf in WordFrequencies(AllWords("pride-and-prejudice.txt")) orderby wf.Value descending select wf).Take(100))
+            {
+                Console.WriteLine("{0} appears {1} times", wordAndFrequency.Key, wordAndFrequency.Value);
+            }
+        }
+    }
+}
