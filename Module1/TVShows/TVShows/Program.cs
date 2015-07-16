7e9b460b5bd5b426e552a589f9fb4c9518a5cf01
Sasha Goldshtein <goldshtn@gmail.com>
2015-07-16 08:49:06 +0300

    Added solution for Module 1 lab

---
+++ Module1/TVShows/TVShows/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVShows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Sopranos");
+            Console.WriteLine("The Big Bang Theory");
+            Console.WriteLine("Suits");
+            Console.WriteLine("Äkta människor");
+            Console.WriteLine("Battlestar Galactica");
+        }
+    }
+}
