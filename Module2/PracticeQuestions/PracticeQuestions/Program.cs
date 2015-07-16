7e89a151d3e12ec21da2088dd4e9d8f4b662ace3
Sasha Goldshtein <goldshtn@gmail.com>
2015-07-16 09:04:20 +0300

    Added practice questions for Module 2

---
+++ Module2/PracticeQuestions/PracticeQuestions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine("**** " + input + " ****");
+
+            input = Console.ReadLine();
+            int number = int.Parse(input);
+            Console.WriteLine(number * number);
+        }
+    }
+}
